49af3b4436e2f042def2cbdaebbe870edc37dfcf
jayasudha12 <123159157+jayasudha12@users.noreply.github.com>
2025-05-29 17:46:35 +0530

    Update server.js

--- ToDoList_Backend/server.js
+++ ToDoList_Backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://taskflowproject.netlify.app/'],
+  origin: ['http://localhost:3000', 'https://taskflowproject.netlify.app'],
   credentials: true, 
 }));
 
