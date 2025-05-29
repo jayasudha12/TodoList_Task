83b71c367c65704d805dca20292ccfdd1d02ab7d
jayasudha12 <123159157+jayasudha12@users.noreply.github.com>
2025-05-29 22:38:55 +0530

    Update Login.js

--- ToDoList_Frontend/src/components/Login.js
+++ ToDoList_Frontend/src/components/Login.js
@@ -23,7 +23,7 @@ function Login() {
   const handleEmailLogin = async (e) => {
   e.preventDefault();
   try {
-    const response = await fetch('https://todolist-project-akxp.onrender.com/auth/login', {
+    const response = await fetch('https://todo-backend-e98t.onrender.com/auth/login', {
       method: 'POST',
       credentials: 'include',
       headers: { 'Content-Type': 'application/json' },
